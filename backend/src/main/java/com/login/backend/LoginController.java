ec92efaa65698c7c38d9912c82925e2928c67947
yuto miyoshi <goodmorningyuto@gmail.com>
2024-05-27 07:24:57 +0900

    UPDATE：データベースとバックエンドの接続を確立

--- backend/src/main/java/com/login/backend/LoginController.java
+++ backend/src/main/java/com/login/backend/LoginController.java
@@ -1,14 +1,22 @@
 package com.login.backend;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.login.backend.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 public class LoginController {
+
+    @Autowired
+    UserRepository userRepository;
     
     @GetMapping("/")
     public String hello() {
+        System.out.println(userRepository.findAll());
         return "hello world";
     }
     
