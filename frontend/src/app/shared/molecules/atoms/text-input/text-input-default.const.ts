6a0f618f20c96cf5bdd61286baf348475d24d0b2
yuto miyoshi <goodmorningyuto@gmail.com>
2024-06-01 11:09:16 +0900

    UPDATE：文字入力部品の作成

---
+++ frontend/src/app/shared/molecules/atoms/text-input/text-input-default.const.ts
@@ -0,0 +1,11 @@
+export namespace TextInputDefaultConst {
+    export const text = '';
+
+    export const title = '';
+
+    export const actionInput = () => {};
+
+    export const actionFocus = () => {};
+
+    export const actionBlur = () => {};
+}
---
+++ frontend/src/app/shared/molecules/atoms/text-input/text-input.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { TextInputDefaultConst } from "./text-input-default.const";
+
+@Component({
+    selector: 'app-text-input',
+    standalone: true,
+    imports: [FormsModule],
+    templateUrl: './text-input.component.html',
+    styleUrl: './text-input.component.scss',
+})
+export class TextInputComponent {
+    /**
+     * Input target
+     */
+    @Input() text = TextInputDefaultConst.text;
+
+    @Input() title = TextInputDefaultConst.title;
+
+    @Input() actionInput = TextInputDefaultConst.actionInput;
+
+    @Input() actionFocus = TextInputDefaultConst.actionFocus;
+
+    @Input() actionBlur = TextInputDefaultConst.actionBlur;
+
+    /**
+     * Focused on Input Form
+     */
+    isFocused = false;
+    
+    onInput(): void {
+        this.actionInput();
+    }
+    
+    onFocus(): void {
+        this.isFocused = true;
+        this.actionFocus();
+    }
+    
+    onBlur(): void {
+        this.isFocused = false;
+        this.actionBlur();
+    }
+
+}
