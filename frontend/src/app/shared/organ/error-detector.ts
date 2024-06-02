f3a42e320f1dd8d2964bc3aaa10f0c2f9ab4d294
yuto miyoshi <goodmorningyuto@gmail.com>
2024-06-02 11:07:31 +0900

    UPDATE：エラー検知クラスを実装（ビルダーパターンで）

---
+++ frontend/src/app/shared/organ/error-detector.ts
@@ -0,0 +1,61 @@
+import { ValueCheck } from '../application-security/value-check';
+import { ErrorDefinition } from '../application-signature/error-definition.interface';
+
+export class ErrorDetector<T, S> {
+  private currentStatus: T;
+
+  private value: S | undefined = undefined;
+
+  constructor(
+    private readonly defList: ErrorDefinition<T, S>[],
+    private readonly noErrorCode: T,
+  ) {
+    this.currentStatus = noErrorCode;
+  }
+
+  start(value: S): this {
+    this.currentStatus = this.noErrorCode;
+    this.value = value;
+    return this;
+  }
+
+  filter(errorCode: T): this {
+    if (ValueCheck.isUndefined(this.value)) {
+      // TODO: assert
+      return this;
+    }
+    if (this.currentStatus !== this.noErrorCode) {
+      return this;
+    }
+
+    const def = this.defList.find(
+      (def: ErrorDefinition<T, S>) => def.code === errorCode,
+    );
+
+    if (ValueCheck.isUndefined(def)) {
+      // TODO: Assert
+      return this;
+    }
+
+    if (def.logic(this.value)) {
+      this.currentStatus = def.code;
+    }
+
+    return this;
+  }
+
+  stop(): void {
+    this.refresh();
+  }
+
+  stopWithResult(): T {
+    const result = this.currentStatus;
+    this.refresh();
+    return result;
+  }
+
+  private refresh(): void {
+    this.currentStatus = this.noErrorCode;
+    this.value = undefined;
+  }
+}
