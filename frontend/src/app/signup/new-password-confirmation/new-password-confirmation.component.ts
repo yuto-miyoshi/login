5bed48b98b8ec9d77423833b9f4a81f306fb9f66
yuto miyoshi <goodmorningyuto@gmail.com>
2024-06-02 20:44:56 +0900

    UPDATE：パスワードと確認用パスワードの照合の動作チェックを実装

--- frontend/src/app/signup/new-password-confirmation/new-password-confirmation.component.ts
+++ frontend/src/app/signup/new-password-confirmation/new-password-confirmation.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { PasswordErrorComponent } from '../../shared/molecules/password-error/password-error.component';
 import { NewPasswordConfirmationDefaultConst } from './new-password-confirmation-default.const';
+import { ErrorDetector } from '../../shared/domain/tool/error-detector';
+import {
+  PasswordErrorCode as Code,
+  passwordErrorDefinitionDictonary as defList,
+  generatePasswordMismatchLogic,
+} from '../../shared/domain/password/password-error.definition';
 
 @Component({
   selector: 'app-new-password-confirmation',
@@ -12,5 +18,32 @@ import { NewPasswordConfirmationDefaultConst } from './new-password-confirmation
 export class NewPasswordConfirmationComponent {
   @Input() password = NewPasswordConfirmationDefaultConst.password;
 
+  @Input() actionIfError = () => {};
+
   passwordConfirm = NewPasswordConfirmationDefaultConst.password;
+
+  errorDetector = new ErrorDetector(defList, Code.noError);
+
+  errorDetectorConfirm = new ErrorDetector(defList, Code.noError);
+
+  onJudgeError = () => {
+    return this.errorDetector
+      .start(this.password)
+      .filter(Code.tooShort)
+      .filter(Code.tooLong)
+      .filter(Code.forbiddenCharacter)
+      .filter(Code.atLeastOneAlphOneNumber)
+      .stopWithResult();
+  };
+
+  onJudgeErrorConfirm = () => {
+    return this.errorDetectorConfirm
+      .start(this.passwordConfirm)
+      .filter(Code.noInput)
+      .filter(
+        Code.mismatch,
+        generatePasswordMismatchLogic(this.passwordConfirm),
+      )
+      .stopWithResult();
+  };
 }
