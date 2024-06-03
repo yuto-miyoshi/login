5b4e39d21fc64b84e63b5eaf801c01c53a005f84
yuto miyoshi <goodmorningyuto@gmail.com>
2024-06-03 21:33:46 +0900

    UPDATE：サインアップコンポーネントのスケルトンを定義

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { StringData } from '../shared/domain/string-data.interface';
 import { SignupDefaultConst } from './signup-default.const';
+import { MailErrorComponent } from '../shared/molecules/mail-error/mail-error.component';
+import { NewPasswordConfirmationComponent } from './new-password-confirmation/new-password-confirmation.component';
+import { ButtonComponent } from '../shared/molecules/atoms/button/button.component';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [
+    MailErrorComponent,
+    NewPasswordConfirmationComponent,
+    ButtonComponent,
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss',
 })
