79524d48e5d41eae6d1090dec50ec6047ee05ff4
yuto miyoshi <goodmorningyuto@gmail.com>
2024-06-03 22:49:24 +0900

    FIX：ルーターパスの修正

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { AccessFailComponent } from './shared/except-page/access-fail/access-fail.component';
 
 export const routes: Routes = [
-  { path: '/error', component: AccessFailComponent },
+  { path: 'error', component: AccessFailComponent },
 ];
