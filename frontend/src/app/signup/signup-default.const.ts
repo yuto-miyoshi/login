32276e65254f497d603e3a21d7a0fb271f9ee3bd
yuto miyoshi <goodmorningyuto@gmail.com>
2024-06-03 22:39:39 +0900

    UPDATE：サインアップコンポーネントの各種バインディングを実装

--- frontend/src/app/signup/signup-default.const.ts
+++ frontend/src/app/signup/signup-default.const.ts
@@ -1,3 +1,11 @@
 export namespace SignupDefaultConst {
   export const title = { text: 'Sign Up' };
+
+  export const mail = { text: '' };
+
+  export const password = { text: '' };
+
+  export const okButton = { text: 'Submit' };
+
+  export const ngButton = { text: 'Resolve Error' };
 }
--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -4,6 +4,13 @@ import { SignupDefaultConst } from './signup-default.const';
 import { MailErrorComponent } from '../shared/molecules/mail-error/mail-error.component';
 import { NewPasswordConfirmationComponent } from './new-password-confirmation/new-password-confirmation.component';
 import { ButtonComponent } from '../shared/molecules/atoms/button/button.component';
+import { MailErrorJudgeSignature } from '../shared/application/error-judge.signature';
+import { ErrorDetector } from '../shared/domain/tool/error-detector';
+import {
+  MailErrorCode as Code,
+  mailErrorDefinitionDictionary as defList,
+} from '../shared/domain/mail/mail-error.definition';
+import { JudgeReactionSignature } from '../shared/application/judge-reaction.signature';
 
 @Component({
   selector: 'app-signup',
@@ -17,5 +24,50 @@ import { ButtonComponent } from '../shared/molecules/atoms/button/button.compone
   styleUrl: './signup.component.scss',
 })
 export class SingupComponent {
-  readonly title: StringData = SignupDefaultConst.title;
+  title: StringData = SignupDefaultConst.title;
+
+  mail: StringData = SignupDefaultConst.mail;
+
+  password: StringData = SignupDefaultConst.password;
+
+  private isErrorMail = false;
+
+  private isErrorPassword = false;
+
+  private errorDetectorMail = new ErrorDetector(defList, Code.noError);
+
+  errorJudgeMail: MailErrorJudgeSignature = () => {
+    return this.errorDetectorMail
+      .start(this.mail.text)
+      .filter(Code.noInput)
+      .filter(Code.tooLong)
+      .filter(Code.atMarkAbsent)
+      .filter(Code.invalidDomainPattern)
+      .filter(Code.shortDomain)
+      .filter(Code.rangeOutTopLevelDomainLength)
+      .filter(Code.forbiddenCharacter)
+      .stopWithResult();
+  };
+
+  onReactJudgeMail: JudgeReactionSignature = (judge: boolean) => {
+    this.isErrorMail = judge;
+  };
+
+  onErrorPassword: JudgeReactionSignature = (judge: boolean) => {
+    this.isErrorPassword = judge;
+  };
+
+  get isError(): boolean {
+    return this.isErrorMail || this.isErrorPassword;
+  }
+
+  get buttonMessage(): StringData {
+    return this.isError
+      ? SignupDefaultConst.ngButton
+      : SignupDefaultConst.okButton;
+  }
+
+  onClickButton(): void {
+    // TODO miyoshi: implements
+  }
 }
