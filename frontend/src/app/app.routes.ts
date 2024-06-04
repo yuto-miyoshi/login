8a2a071c5736373549244e649224e5a1a6f9d379
yuto miyoshi <goodmorningyuto@gmail.com>
2024-06-04 23:52:22 +0900

    UPDATE：ログインコンポーネントのルーティングを実装

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AccessFailComponent } from './shared/except-page/access-fail/access-fail.component';
+import { LoginComponent } from './login/login.component';
 import { SingupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
-  { path: '', component: SingupComponent },
+  { path: '', component: LoginComponent },
+  { path: 'signup', component: SingupComponent },
   { path: 'error', component: AccessFailComponent },
 ];
