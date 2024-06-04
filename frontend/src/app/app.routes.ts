f06de03fca0b78c1cbb2a7fce306b88975252ce1
yuto miyoshi <goodmorningyuto@gmail.com>
2024-06-04 23:16:12 +0900

    UPDATE：ルーターを修正

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AccessFailComponent } from './shared/except-page/access-fail/access-fail.component';
+import { SingupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
+  { path: '', component: SingupComponent },
   { path: 'error', component: AccessFailComponent },
 ];
