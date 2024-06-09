c67df43ef908187e0f22c6dcf4b6767a11336f5e
yuto miyoshi <goodmorningyuto@gmail.com>
2024-06-09 18:56:39 +0900

    ADD：ログイン情報の表示エリアの作成

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LoginDataFetchService } from './login-data-fetch.service';
 import { Subscription } from 'rxjs';
+import { LoginDataAreaComponent } from './login-data-area/login-data-area.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
+  imports: [LoginDataAreaComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
---
+++ frontend/src/app/dashboard/login-data-area/login-data-area.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-data-area',
+  standalone: true,
+  templateUrl: './login-data-area.component.html',
+  styleUrl: './login-data-area.component.scss',
+})
+export class LoginDataAreaComponent {}
