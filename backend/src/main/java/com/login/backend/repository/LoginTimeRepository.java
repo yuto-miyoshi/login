57e4a91d7aa941a386dc5bc2c7e3adbbaae0bc6b
yuto miyoshi <goodmorningyuto@gmail.com>
2024-06-09 09:45:36 +0900

    UPDATE：ログイン日時を登録するCRUDを実装

--- backend/src/main/java/com/login/backend/repository/LoginTimeRepository.java
+++ backend/src/main/java/com/login/backend/repository/LoginTimeRepository.java
@@ -1,9 +1,18 @@
 package com.login.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.login.backend.entity.LoginTime;
 
 @Repository
-public interface LoginTimeRepository extends JpaRepository<LoginTime, Long> {}
+public interface LoginTimeRepository extends JpaRepository<LoginTime, Long> {
+
+    @Query(value="select * from login_time where user_id = (select user_id from users where mail = :mail)", nativeQuery=true)
+    public List<LoginTime> loginRecords(@Param("mail") String mail);
+    
+}
