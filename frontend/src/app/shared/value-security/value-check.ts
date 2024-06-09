72ae2fc928f135665a7498dfe2dcedc041c844dd
yuto miyoshi <goodmorningyuto@gmail.com>
2024-06-09 14:15:11 +0900

    ADD：フロントエンドでNULL判定用utilを実装

--- frontend/src/app/shared/value-security/value-check.ts
+++ frontend/src/app/shared/value-security/value-check.ts
@@ -2,4 +2,8 @@ export namespace ValueCheck {
   export const isUndefined = (value: unknown): value is undefined => {
     return value === undefined;
   };
+
+  export const isNull = (value: unknown): value is null => {
+    return value === null;
+  };
 }
