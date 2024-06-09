491d3a562d34054f9a0aa257ec22e63628b37d6c
yuto miyoshi <goodmorningyuto@gmail.com>
2024-06-09 12:07:34 +0900

    UPDATE：ログイン日時のINSERT文をクエリベースに変更

--- backend/src/main/java/com/login/backend/LoginRecordService.java
+++ backend/src/main/java/com/login/backend/LoginRecordService.java
@@ -17,16 +17,7 @@ public class LoginRecordService {
     LoginTimeRepository loginTimeRepository;
 
     public void record(LoginChallenge form) {
-        LoginTime loginTime = new LoginTime();
-        User user = new User();
-
-        user.setMail(form.mail);
-        user.setPassword(form.password);
-
-        loginTime.setLoginTime(LocalDateTime.now());
-        loginTime.setUser(user);
-
-        this.loginTimeRepository.save(loginTime);
+        this.loginTimeRepository.insertLoginRecord(form.mail, LocalDateTime.now());
     }
 
 }
--- backend/src/main/java/com/login/backend/repository/LoginTimeRepository.java
+++ backend/src/main/java/com/login/backend/repository/LoginTimeRepository.java
@@ -1,5 +1,6 @@
 package com.login.backend.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,5 +15,8 @@ public interface LoginTimeRepository extends JpaRepository<LoginTime, Long> {
 
     @Query(value="select * from login_time where user_id = (select user_id from users where mail = :mail)", nativeQuery=true)
     public List<LoginTime> loginHistory(@Param("mail") String mail);
+
+    @Query(value="insert into login_time(user_id, login_time) values ((select id from users where mail=:mail), :login_time)", nativeQuery=true)
+    public void insertLoginRecord(@Param("mail") String mail, @Param("login_time") LocalDateTime time);
 
 }
