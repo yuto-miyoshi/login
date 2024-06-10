fe379e7f4b42fbd42fe08278620884c35a525285
yuto miyoshi <goodmorningyuto@gmail.com>
2024-06-10 21:20:07 +0900

    UPDATE：データベースにダミーデータを追加

--- database/forDocker/db/initdb/3_insert_init_user.sql
+++ database/forDocker/db/initdb/3_insert_init_user.sql
@@ -2,6 +2,6 @@ INSERT INTO users (
   mail,
   password
 ) values ( 
-  'seanayuto0826@gmail.com',
+  'seanayuuto0826@gmail.com',
   'helloworld'
 );
--- database/forDocker/db/initdb/4_insert_init_login_time.sql
+++ database/forDocker/db/initdb/4_insert_init_login_time.sql
@@ -5,3 +5,243 @@ INSERT INTO login_time (
     1,
     '2022-01-01 00:00:00'
 );
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-10 10:10:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-09 10:10:00'
+);
+
+INSERT INTO login_time (
+    user_id
+    login_time
+) VALUES (
+    1,
+    '2024-06-07 10:15:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-07 10:20:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-07 10:21:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-07 10:10:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-07 10:05:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-07 10:00:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-07 10:00:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-07 10:00:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-07 10:00:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-07 10:00:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-07 10:00:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-07 10:00:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-07 10:00:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-07 10:00:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-05 10:12:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-05 10:12:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-05 10:12:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-05 10:12:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-06-05 10:12:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-05-05 10:12:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-05-05 10:12:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-05-05 10:12:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-05-05 10:12:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-05-05 10:12:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-05-05 10:12:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-05-05 10:12:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-05-05 10:12:00'
+);
+
+INSERT INTO login_time (
+    user_id,
+    login_time
+) VALUES (
+    1,
+    '2024-04-05 10:12:00'
+);
